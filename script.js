e453b71d37b762783884d3fe9a1436a1ffe9de21
AdityaAshish <81067181+AdityaAshish@users.noreply.github.com>
2021-10-08 17:25:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+canvas=document.getElementById("Canvas");
+ctx = canvas.getContext("2d");
+color = "blue";
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth = 4;
+ctx.arc(200, 150, 60, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById("Canvas");
+ctx = canvas.getContext("2d");
+color = "yellow";
+ctx.beginPath();
+ctx.strokeStyle= "yellow"
+ctx.lineWidth = 4;
+ctx.arc(250, 225, 60, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById("Canvas");
+ctx = canvas.getContext("2d");
+color = "black";
+ctx.beginPath();
+ctx.strokeStyle= "black"
+ctx.lineWidth = 4;
+ctx.arc(300, 150, 60, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById("Canvas");
+ctx = canvas.getContext("2d");
+color = "green";
+ctx.beginPath();
+ctx.strokeStyle= "green"
+ctx.lineWidth = 4;
+ctx.arc(350, 225, 60, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas=document.getElementById("Canvas");
+ctx = canvas.getContext("2d");
+color = "red";
+ctx.beginPath();
+ctx.strokeStyle= "red"
+ctx.lineWidth = 4;
+ctx.arc(400, 150, 60, 0, 2*Math.PI);
+ctx.stroke();
+
+
+
+
+
+
+
+
